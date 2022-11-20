784204541929030572a35dfe550e2f5d7b4aadcc
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-11-19 23:16:53 -0800

    tried stuff

--- select/select_server.py
+++ select/select_server.py
@@ -6,9 +6,42 @@ import sys
 import socket
 import select
 
+
+
 def run_server(port):
-    # TODO--fill this in
-    pass
+
+    ready_set = {}
+    addr = ('127.0.0.1', port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', port))
+    s.listen()
+    client, addr = s.accept()
+    print('(\'127.0.0.1\', ' + str(port) + '): connected')
+    print(s)
+
+    ready_set = {s}
+    print("hi0")
+    ready_to_read, _, _ = select.select(ready_set, {}, {})
+    print('hi1')
+    for s in ready_to_read:
+        data = s.recv(port)
+        print("this is data" + str(data))
+
+
+    while True:
+        response = client.recv(port).decode()
+
+        print(len(response))
+
+        if len(response) <= 0:
+            client.close()
+            s.close()
+            print("hi")
+            break
+
+        print(response)
+
+
 
 #--------------------------------#
 # Do not modify below this line! #
